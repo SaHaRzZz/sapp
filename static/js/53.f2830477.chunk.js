a6912004307b084d5b40c4e6e03bd80ceb2ed22e
DESKTOP-UNVH3TB\Sahar <imsaharhayon@gmail.com>
2021-02-28 09:22:00 +0200

    Updates

--- static/js/53.f2830477.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsapp=this.webpackJsonpsapp||[]).push([[53],{187:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return i}));var o=e(18),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,i=document.elementFromPoint(t/2,e/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.f2830477.chunk.js.map
